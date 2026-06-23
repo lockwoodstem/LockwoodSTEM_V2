576c6b17534ec903c4abc243a3df123a9a6bfe70
lockwoodstem <jonathan@lockwoodstem.org>
2026-06-22 21:33:14 -0400

    Update WEB_APP_URL for certification system

--- certifications/auth-config.js
+++ certifications/auth-config.js
@@ -2,7 +2,7 @@
 // Paste your deployed Google Apps Script Web App URL below.
 // Example: https://script.google.com/macros/s/AKfycb.../exec
 window.LOCKWOOD_CERT_AUTH = {
-  WEB_APP_URL: "",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyHs0w2mCCZykn8bagbNqcOgejCj5Deu8KafnndwNOCkGfAwyRHlbUUwhOXX7JYnzM3/exec",
   REQUIRE_BACKEND: true,
   SESSION_KEY: "lockwoodstem_cert_session",
   PROFILE_KEY: "lockwoodstem_cert_profile"
